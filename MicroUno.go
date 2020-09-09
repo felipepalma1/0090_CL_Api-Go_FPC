f81eb66eb7047b329368363a4d42c194284c7e24
Felipe Palma <felipepalmacastro@gmail.com>
2020-09-09 12:52:24 -0300

    Agregado comentarios

--- MicroUno.go
+++ MicroUno.go
@@ -1,3 +1,7 @@
+/* Versión mínima de un micro servicio en Go. Utiliza la función http.HandleFunc para poder
+enviar un mensaje por consola cada vez que es activado. Ni siquiera le maanda un mensaje al cliente.
+Escucha en el puerto 9090*/
+
 package main
 
 import (
@@ -7,9 +11,12 @@ import (
 
 func main() {
 
+	/*Handler básico que envía un mensaje por consola desde el lado del servidor cada vez que un
+	cliente solicita el recurso raíz*/
 	http.HandleFunc("/", func(http.ResponseWriter, *http.Request) {
 		log.Println("Hola Mundo")
 	})
 
+	/*Aquí en el argumento nil es donde debe ir el Handler de la solicitud*/
 	http.ListenAndServe(":9090", nil)
 }
